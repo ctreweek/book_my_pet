7e3b6a076c924d1f92f65970df2415c0ec58e6d3
Courtney Treweek <c.k.treweek@gmail.com>
2017-08-06 11:12:33 -0400

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,38 @@
-User.destroy_all
+Booking.destroy_all
 Dog.destroy_all
+User.destroy_all
 
 puts "creating seeds"
 
+@users = []
 10.times do
-  user = User.new(
+  @users << User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
     email: Faker::Internet.free_email,
     password: "1234567890"
   )
 
-  user.save!
 end
 
-dog = Dog.new({ name: 'Louis', breed: 'Pug', description: 'Such a fun pup!', age: 6})
-dog.user = user
-dog.save!
+# dog = Dog.new({ name: 'Louis', breed: 'Pug', description: 'Such a fun pup!', age: 6})
+# dog.user = user
+# dog.save!
+
+
+@breeds = ["black lab", "yellow lab", "pug", "huskey", "shih tzu", "boston terrier"]
+9.times do
+  url = "http://lorempixel.com/1600/600/animals/" + rand(1..10).to_s
+  dog = Dog.new ({
+    name: Faker::Name.first_name,
+    breed: @breeds.sample ,
+    description: "Fun loving dog! Hobbies include playing fetch and long walks. I'm a snuggler!",
+    age: rand(0..12)
+    })
+  dog.user_id = User.all.sample.id
+  dog.save!
+  dog.photo_url = url
+end
+
 puts "done!"
+
