6cad92a5b701df4c92dac74963b22a4cbb8ad274
Courtney Treweek <c.k.treweek@gmail.com>
2017-08-07 19:56:04 -0400

    Put book a new appointment for in modal on dog show, add booking to show method in dog controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ User.destroy_all
 puts "creating seeds"
 
 @users = []
-10.times do
+12.times do
   @users << User.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
@@ -21,8 +21,8 @@ end
 
 
 @breeds = ["black lab", "yellow lab", "pug", "huskey", "shih tzu", "boston terrier"]
-9.times do
-  url = "http://lorempixel.com/1600/600/animals/" + rand(1..10).to_s
+12.times do
+  url = "http://loremflickr.com/320/240/dog"
   dog = Dog.new ({
     name: Faker::Name.first_name,
     breed: @breeds.sample ,
